725c274a47c5aeae006b86c0e19b6f6b3dec2ce4
Prasannapappu <77119412+Prasannapappu@users.noreply.github.com>
2021-01-07 23:14:46 +0530

    Add files via upload

---
+++ 7.1.21.py
@@ -0,0 +1,8 @@
+a=input("Simple Interest")
+p=int(input("Enter the value of P="))
+n=int(input("Enter the value of N="))
+r=int(input("Enter the value of R="))
+x=p*n*r//100
+b=input("FORMULA")
+C=input("Simple Inerest = PNR/100")
+print("SI = ",x)
